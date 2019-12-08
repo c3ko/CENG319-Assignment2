72eb502ed573f8fd5129d28780719a91c27ddab2
Mohamed K <mohamedkore@gmail.com>
2019-12-08 16:41:06 -0500

    Added AnalogClock to MainActivity

--- app/src/main/java/com/ceng317/assignment2/MainActivity.java
+++ app/src/main/java/com/ceng317/assignment2/MainActivity.java
@@ -3,12 +3,16 @@ package com.ceng317.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.AnalogClock;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AnalogClock clock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
